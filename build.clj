a344b0867d8eb543e689628329ca754b317c4c1d
Casey Link <unnamedrambler@gmail.com>
2025-08-13 15:53:29 +0200

    Prepare eventual build.clj

---
+++ build.clj
@@ -0,0 +1,88 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [clojure.edn :as edn]
+            [clojure.string :as str]))
+
+(def project (-> (edn/read-string (slurp "deps.edn")) :aliases :neil :project))
+(def cwd (-> (java.io.File. ".")  .getCanonicalFile .getName))
+(def rev (str/trim (b/git-process {:git-args "rev-parse HEAD"})))
+(def lib (:name project))
+(def version (:version project))
+(def license-id (-> project :license :id))
+(def license-file (or (-> project :license :file) "LICENSE"))
+(def description (:description project))
+(def repo-url-prefix (:url project (-> (b/git-process {:git-args "remote get-url origin"})
+                                       (str/trim)
+                                       (str/replace #"\.git$" ""))))
+(def scm (:scm project "git"))
+(assert lib ":name must be set in deps.edn under the :neil alias")
+(assert version ":version must be set in deps.edn under the :neil alias")
+(assert description ":description must be set in deps.edn under the :neil alias")
+(assert license-id "[:license :id] must be set in deps.edn under the :neil alias")
+(def class-dir "target/classes")
+(def basis_ (delay (b/create-basis {:project "deps.edn"})))
+(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn permalink [subpath]
+  (str repo-url-prefix "/blob/" rev "/" subpath))
+
+(defn url->scm [url-string]
+  (let [[_ domain repo-path] (re-find #"https?://?([\w\-\.]+)/(.+)" url-string)]
+    [:scm
+     [:url (str "https://" domain "/")]
+     [:connection (str "scm:git:https://" domain "/" repo-path)]
+     [:developerConnection (str "scm:git:ssh:git@" domain ":" repo-path)]]))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis @basis_
+                :src-dirs ["src"]
+                :pom-data [[:description description]
+                           [:url repo-url-prefix]
+                           [:licenses
+                            [:license
+                             [:name license-id]
+                             [:url (permalink license-file)]]]
+                           (conj (url->scm repo-url-prefix) [:tag (str "v" version)])]})
+
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn install [_]
+  (jar {})
+  (b/install {:basis @basis_
+              :lib lib
+              :version version
+              :jar-file jar-file
+              :class-dir class-dir}))
+
+(defn uber [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src" "resources"] :target-dir class-dir})
+  (b/compile-clj {:basis @basis_
+                  :src-dirs ["src"]
+                  :class-dir class-dir
+                  :java-opts ;; needed for coffi
+                  ["--enable-native-access=ALL-UNNAMED"]})
+
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis @basis_
+           :manifest  {"Enable-Native-Access" "ALL-UNNAMED"}}))
+
+(defn deploy [opts]
+  (jar opts)
+  ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
+   (merge {:installer :remote
+           :artifact jar-file
+           :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
+          opts))
+  opts)
